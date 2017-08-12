40d22762ca04493b36bed7e6ac8a613eeafcb5b5
zhouyouz <zhouyouz.163.com>
2017-08-12 16:00:29 +0800

    add java directory

--- activeMQ/src/main/java/com.szkingdom/consumer/ActiveMQConsumer.java
+++ activeMQ/src/main/java/com.szkingdom/consumer/ActiveMQConsumer.java
@@ -1,6 +1,7 @@
-package productor;
+package com.szkingdom.consumer;
 
-import org.apache.activemq.*;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
@@ -8,26 +9,26 @@ import javax.jms.*;
  * Copyright (C) 1998-至今, SHENZHEN KINGDOM Co., Ltd.
  * All Rights Reserved.
  * ===========================================================================
- * 模块名称：消息生产者
+ * 模块名称：消息消费模块
  * 功能说明：
  * 作    者：zhouyou01
- * 创建日期：2017-08-05 13:17
+ * 创建日期：2017-08-05 13:54
  * 版 本 号：
  * 修改历史：
  * <p/>
  * 修改日期                  姓名                         内容
  * ---------------------------------------------------------------------------
  *******************************************************************************/
-public class ActiveMQProducer {
+public class ActiveMQConsumer {
     private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
     private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
 
     //默认的连接地址
-    private static final String BROKER_URL = "tcp://localhost:61616";
+    private static final String BROKER_URL = ActiveMQConnection.DEFAULT_BROKER_URL;
 
     public static void main(String[] args) {
         //1、创建连接工厂
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKER_URL);
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKER_URL);
         Connection connection = null;
         Session session = null;
         try {
@@ -36,27 +37,20 @@ public class ActiveMQProducer {
             //3、启动连接
             connection.start();
             //4、创建session，使用自动确认的方式
-            session = connection.createSession(Boolean.TRUE,Session.AUTO_ACKNOWLEDGE);
-            //5、创建消息目的地。P2P消息模式中目的地是一个队列
+            session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+            //5、创建消息目的地，连接消息队列
             Destination destination = session.createQueue("firstQueue");
-            //6、创建生产者
-            MessageProducer producer =session.createProducer(destination);
-            //7、发送消息
-            for (int i=0;i<10;i++){
-                TextMessage message = session.createTextMessage("发送消息到ActiveMQ"+i);
-                producer.send(message);
-                System.out.println(message.getText());
+            //6、创建消费者
+            MessageConsumer consumer = session.createConsumer(destination);
+            //7、接收消息
+            while (true) {
+                TextMessage textMessage = (TextMessage) consumer.receive(3000);
+                if (textMessage!=null){
+                    System.out.println(textMessage.getText());
+                }
             }
-            session.commit();
         } catch (JMSException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                session.close();
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
         }
-
     }
 }
